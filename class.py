cf74db966a33b57eda4f43fde3708e7fc0f83c54
jdwaldman55 <jdwaldman55@gmail.com>
2020-07-09 10:21:40 -0400

    Create class.py
    Add python classes with inheritance

---
+++ class.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    doors = 0
+    energy_source= ''
+    capacity = 0
+    color = ''
+
+class Plane(Vehicle):
+    number_of_wings = 2
+    engines = 4
+
+class Car(Vehicle):
+    seat_meterial = 'Leather'
+    steering_wheel_side = 'Left'
+    
